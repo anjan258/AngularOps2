0ca927d88a2e33c3989b92c53b5fab888ee2695a
aavula25 <aavula@channelassist.com>
2020-07-26 13:12:05 -0400

    tracking form group and form control changes

--- src/app/employee/employee-create.component.ts
+++ src/app/employee/employee-create.component.ts
@@ -16,6 +16,7 @@ export class EmployeeCreateComponent implements OnInit {
   // we need to import "ReactiveFormsModule" -- in app-module.ts in order to these forms to work
 
   employeeForm: FormGroup;
+  fullNameLength: number;
 
   // creating instance of FormBuilder for "Angular Formbuilder" - code
   constructor(private fb: FormBuilder) { }
@@ -46,7 +47,7 @@ export class EmployeeCreateComponent implements OnInit {
     // [''] -- setting null/empty as default value, can set to any value if required.
     // for an example we have set a default value like - proficiency: ['beginner']
     // fullName: ['', Validators.required] -- adds required validation
-    //  [Validators.required, Validators.maxLength(10)] -- for multiple validations
+    // [Validators.required, Validators.maxLength(10)] -- for multiple validations
 
     this.employeeForm = this.fb.group({
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
@@ -57,8 +58,52 @@ export class EmployeeCreateComponent implements OnInit {
         proficiency: ['beginner']
       })
     });
+
+
+    // to subscribe/track the changes of any form control
+    this.employeeForm.get('fullName').valueChanges.subscribe((value: string) => {
+      this.fullNameLength = value.length;
+    });
+
+        // to subscribe/track the changes of any form group
+    this.employeeForm.valueChanges.subscribe((value: string) => {
+            console.log(JSON.stringify(value));
+        });
+
+     // to subscribe/track the changes of any nested/subset form group
+     // 'skills' -- sub/nested group in employeeForm - group
+    this.employeeForm.get('skills').valueChanges.subscribe((value: string) => {
+      console.log(JSON.stringify(value));
+  });
+
   }
 
+    // loop through all the form controls in form group
+    logKeyValueFormGroupPairs(grp: FormGroup): void{
+      console.log(Object.keys(grp.controls));
+    }
+
+        // loop through all the form controls in form group and nested groups and controls
+        logKeyValueFormGroupAndNestedGroupPairs(grp: FormGroup): void{
+         Object.keys(grp.controls).forEach((key: string) =>{
+
+          const abstractControl = grp.get(key);
+          if (abstractControl instanceof FormGroup){
+             // recursive calling to loop through control if instacne is FormGroup
+            this.logKeyValueFormGroupAndNestedGroupPairs(abstractControl);
+          }
+          else{
+            // else log all the key value pairs of form controls
+            console.log('Key =' + key + ' Value =' + abstractControl.value);
+            // abstractControl.disable(); // to disable to all form controls
+            // abstractControl.markAllAsTouched(); //  marks all controls as touched
+            // abstractControl.markAsDirty();
+            // abstractControl.markAsPending();
+            // abstractControl.markAsPristine();
+            // abstractControl.markAsUntouched();
+          }
+         });
+        }
 
 
   onFormSubmit(): void{
@@ -79,6 +124,9 @@ export class EmployeeCreateComponent implements OnInit {
         proficiency: 'beginner'
       }
     });
+
+    this.logKeyValueFormGroupPairs(this.employeeForm);
+    this.logKeyValueFormGroupAndNestedGroupPairs(this.employeeForm);
   }
 
   // patchValue()-- used to update(all or selected/subset fields/controls)
