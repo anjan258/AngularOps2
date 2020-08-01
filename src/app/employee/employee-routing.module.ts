5c4b0c3b2bfa60c564a0707a65d1919e6f67d81a
aavula25 <aavula@channelassist.com>
2020-08-01 16:36:17 -0400

    routing notes

--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -8,6 +8,9 @@ import { ListEmployeesComponent } from './list-employees.component';
 // so for that we do as follows by creating a root path and adding rest of routes are children
 // so now the urls will be employees, employees/create and employees/create/2
 // also we need to change the routerlink in the app.component.html as (routerLink="employees", routerLink="employees/create")
+
+ //    { path: 'employees', children: [ -- called as component-less route, as it have no components attached
+ // and it has children with common url prefix(called 'employees') with different components
 const routes: Routes = [
     { path: 'employees', children: [
 
