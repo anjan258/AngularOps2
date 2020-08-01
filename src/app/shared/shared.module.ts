b9a15db198ff68451f78c9953ef9f47ae51622d2
aavula25 <aavula@channelassist.com>
2020-08-01 16:17:54 -0400

    shared module notes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule  } from '@angular/forms';
 
 
-
+// shared modules cant have any any providers,
+// we usrer provides to specify services, so if we have multiple providers,
+// we may end up creating mutiple instances of services which breaks the rule if angulars singleton service
+// here we cannot import/export that have providers.
 @NgModule({
   declarations: [],
   // we can import "CommonModule" - module, if we have any other components/modules dependent on this
